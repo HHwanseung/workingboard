b6dcbf3e3b12a1b198ee644c4dc2bc02e0b78e02
seunghwanLee <dkqlch@naver.com>
2022-10-13 16:51:20 +0900

    회원수정

--- src/main/java/com/workboard/workingboard/controller/api/UserApiController.java
+++ src/main/java/com/workboard/workingboard/controller/api/UserApiController.java
@@ -11,16 +11,18 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
 public class UserApiController {
 
     private final UserService userService;
 
+    /**
+     * 회원가입 API
+     */
     @PostMapping("/auth/api/v1/user")
-    public Long save(@RequestBody @Valid UserSaveRequestDto userSaveRequestDto) throws Exception {
+    public Long save(@RequestBody UserSaveRequestDto userSaveRequestDto) {
         return userService.save(userSaveRequestDto.toEntity());
     }
 
@@ -28,10 +30,10 @@ public class UserApiController {
      * 회원수정 API
      */
 
-//    @PutMapping("/api/v1/user")
-//    public Long update(@RequestBody User user, @AuthenticationPrincipal PrincipalDetail principalDetail) {
-//        userService.update(user, principalDetail);
-//        return user.getId();
-//    }
+    @PutMapping("/api/v1/user")
+    public Long update(@RequestBody User user, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        userService.update(user, principalDetail);
+        return user.getId();
+    }
 
 }

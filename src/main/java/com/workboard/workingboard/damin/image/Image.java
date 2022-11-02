9cc8a3500bd0510325596f9305913abae8a89b5c
seunghwanLee <dkqlch@naver.com>
2022-11-02 19:13:51 +0900

    첨부파일

---
+++ src/main/java/com/workboard/workingboard/damin/image/Image.java
@@ -0,0 +1,28 @@
+package com.workboard.workingboard.damin.image;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.workboard.workingboard.damin.user.user.User;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String location;
+
+    private String caption;
+
+    private String postImage;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"password", "images"})
+    private User user;
+
+
+}
